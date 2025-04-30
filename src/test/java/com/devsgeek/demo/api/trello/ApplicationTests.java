7a4b01bd003622721f2258853b53e09d4f566765
Juan Pablo David <hola.devsgeek@gmail.com>
2025-04-30 09:58:53 -0300

    Initial commit

---
+++ src/test/java/com/devsgeek/demo/api/trello/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsgeek.demo.api.trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
