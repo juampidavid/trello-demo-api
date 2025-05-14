fea94b5cdff29a2311af56d9ff311fc0635b10dd
Juan Pablo David <juanpablo@reupliving.com>
2025-05-14 10:21:49 -0300

    Modifying

--- .aws/scripts/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install CodeDeploy Agent
-yum install -y ruby wget
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-./install auto
