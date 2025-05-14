990e38d2084796d4b5eb2090e655fa003fec07bb
Juan Pablo David <juanpablo@reupliving.com>
2025-05-14 15:31:06 -0300

    Secrets key

--- aws-code-deploy/production/after-install.sh
+++ aws-code-deploy/production/after-install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "[Production] Muuuuthafuuuckeeer!!! After install script started" >> /tmp/codedeploy.log
+echo "[Production] After install script started" >> /tmp/codedeploy.log
+
+SECRET_LOCATION="trello-app/production"
+
+# Fetch secrets from AWS Secrets Manager
+SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id $SECRET_LOCATION --query SecretString --output text)
+echo $SECRET_STRING >> /tmp/codedeploy.log
--- aws-code-deploy/staging/after-install.sh
+++ aws-code-deploy/staging/after-install.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "[Staging] After install script started" >> /tmp/codedeploy.log
+
+SECRET_LOCATION="trello-app/staging"
+
+# Fetch secrets from AWS Secrets Manager
+SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id $SECRET_LOCATION --query SecretString --output text)
+echo $SECRET_STRING >> /tmp/codedeploy.log
