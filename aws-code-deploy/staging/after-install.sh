d4761c57897db256cd8ca168a4719a613d6e3208
Juan Pablo David <juanpablo@reupliving.com>
2025-05-14 16:17:04 -0300

    Continue

--- aws-code-deploy/staging/after-install.sh
+++ aws-code-deploy/staging/after-install.sh
@@ -1,9 +1,38 @@
 #!/bin/bash
 
-echo "[Staging] After install script started 123" >> /tmp/codedeploy.log
-
 SECRET_LOCATION="trello-app/staging"
+REQUIRED_VARS=("TRELLO_API_KEY" "TRELLO_TOKEN" "TRELLO_BOARD_ID" "TRELLO_LIST_ID")
+
+
+echo "[Staging] After install script started 123" >> /tmp/codedeploy.log
 
 # Fetch secrets from AWS Secrets Manager
 SECRET_STRING=$(aws secretsmanager get-secret-value --secret-id $SECRET_LOCATION --query SecretString --output text)
 echo $SECRET_STRING >> /tmp/codedeploy.log
+
+# Export secrets as environment variables
+echo $SECRET_STRING | jq -r 'to_entries | .[] | "\(.key)=\(.value)"' >> /etc/sysconfig/trello-app-environments
+
+# Load the environment variables
+source /etc/sysconfig/trello-app-environments
+
+# Verify environment variables are set
+for VAR in "${REQUIRED_VARS[@]}"; do
+    if [ -z "${!VAR}" ]; then
+        echo "Error: $VAR variable are not defined in AWS Secrets Manager." >> /tmp/codedeploy.log
+        exit 1
+    fi
+done
+
+echo "Environment variables successfully set." >> /tmp/codedeploy.log
+
+# Verify Java installation
+echo "Verifying Java installation..." >> /tmp/codedeploy.log
+if java -version 2>&1 | grep -q "openjdk version"; then
+    echo "Java is already installed." >> /tmp/codedeploy.log
+else
+    echo "Installing Java..." >> /tmp/codedeploy.log
+    yum install -y java-22-amazon-corretto
+fi
+
+echo "After-install steps completed." >> /tmp/codedeploy.log
--- src/main/java/com/devsgeek/demo/api/trello/controllers/MainController.java
+++ src/main/java/com/devsgeek/demo/api/trello/controllers/MainController.java
@@ -4,6 +4,7 @@ import com.devsgeek.demo.api.trello.models.dto.CardModelDTO;
 import com.devsgeek.demo.api.trello.services.TrelloServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,11 @@ public class MainController {
 
     @Autowired
     private TrelloServices trelloServices;
+
+    @GetMapping("/hello")
+    public ResponseEntity<?> Hello() {
+        return ResponseEntity.ok("Hello World");
+    }
 
     @PostMapping("/post")
     public ResponseEntity<?> Post(@RequestBody CardModelDTO card) {
