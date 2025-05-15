8605ec4dfdfa2bebca4c953a7a120a22a55fed3e
Juan Pablo David <juanpablo@reupliving.com>
2025-05-15 11:41:41 -0300

    Changes

--- .aws/code-deploy/staging/after-install.sh
+++ .aws/code-deploy/staging/after-install.sh
@@ -47,7 +47,31 @@ else
 fi
 
 
+echo "[Staging] Generating Service file" >> /tmp/codedeploy.log
 
-sudo bash create-service.sh
+FILE_PATH="/etc/systemd/system/trello-api.service"
+
+if [ ! -f "$FILE_PATH" ]; then
+    echo "[Staging] Generating file... $FILE_PATH" >> /tmp/codedeploy.log
+    cat <<EOL > "$FILE_PATH"
+[Unit]
+Description=Trello Api
+
+[Service]
+EnvironmentFile=/etc/sysconfig/trello-app-environments
+ExecStart=/usr/bin/java -jar --enable-preview /home/ec2-user/trello-api/api.jar
+SuccessExitStatus=143
+StandardOutput=append:/var/log/trello-api/stdout.log
+StandardError=append:/var/log/trello-api/stderr.log
+
+[Install]
+WantedBy=multi-user.target
+EOL
+    echo "[Staging] File created successfully." >> /tmp/codedeploy.log
+else
+    echo "[Staging] File already exists. Skipping creation." >> /tmp/codedeploy.log
+fi
+
+
 
 echo "After-install steps completed." >> /tmp/codedeploy.log
